5ac7910e07ad7715d1d2a1d5fe24351d533d619c
Muhammad Kashif <60599437+mhdkashif07@users.noreply.github.com>
2023-11-06 22:54:37 +0500

    add CRUD for category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -22,3 +22,33 @@ exports.getSingleCategory = async (req, res, next) => {
   }
   successResponse(req, res, 'success', OK_CODE, 'custom message', doc);
 };
+
+//** get all categories
+exports.getCategories = async (req, res, next) => {
+  const doc = await Category.find();
+  if (!doc) {
+    // return next(new AppError(`No found with that ID`, 404));
+    return next(new AppError('No documents found', 404));
+  }
+  successResponse(req, res, 'success', 202, 'custom message', doc);
+};
+
+//** delete single category
+exports.deleteCategory = async (req, res, next) => {
+  const doc = await Category.findByIdAndDelete(req.params.id);
+  if (!doc) {
+    // return next(new AppError(`No found with that ID`, 404));
+    return next(new AppError('No document found with that', 404));
+  }
+  successResponse(req, res, 'success', 200, 'Deleted Successfully');
+};
+
+//** update single category
+exports.updateCategory = async (req, res, next) => {
+  const doc = await Category.findByIdAndUpdate(req.params.id, req.body);
+  if (!doc) {
+    // return next(new AppError(`No found with that ID`, 404));
+    return next(new AppError('No document found with that id', 404));
+  }
+  successResponse(req, res, 'success', 200, 'Updated Successfully', doc);
+};
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
 const categoryController = require('../controllers/categoryController');
-// const { gettingError } = require('../Middleware/gettingError');
 
 const router = express.Router();
 
-router.route('/').post(categoryController.createCategory);
+router
+  .route('/')
+  .get(categoryController.getCategories)
+  .post(categoryController.createCategory);
 
-router.route('/:id').get(categoryController.getSingleCategory);
+router
+  .route('/:id')
+  .get(categoryController.getSingleCategory)
+  .delete(categoryController.deleteCategory)
+  .patch(categoryController.updateCategory);
 
 module.exports = router;
