511cf44f9807657404d1c078dc25ae9b26af8ff8
Muhammad Kashif <mhdkashifkhan028@gmail.com>
2023-11-30 17:05:33 +0500

    article schema

--- models/articleModel.js
+++ models/articleModel.js
@@ -3,6 +3,56 @@ const mongoose = require('mongoose');
 
 const articleSchema = mongoose.Schema(
   {
+    scope: {
+      type: String
+    },
+    slug: {
+      type: String,
+      default: null
+    },
+    type: {
+      type: String
+    },
+    provider: {
+      type: String
+    },
+    title: {
+      type: String,
+      default: null
+    },
+    show_in_feed: {
+      type: Boolean,
+      default: true
+    },
+    is_published: {
+      type: Boolean,
+      default: false
+    },
+    reaction_count: {
+      type: String
+    },
+    comment_count: {
+      type: String
+    },
+    score: {
+      type: String
+    },
+    url_hash: {
+      type: String
+    },
+    url_path: {
+      type: String
+    },
+    created_at: {
+      type: String
+    },
+    updated_at: {
+      type: String
+    },
+    published_at: {
+      type: Date,
+      default: null
+    },
     title: {
       type: String,
       required: [true, 'A article must have a name'],
@@ -16,8 +66,7 @@ const articleSchema = mongoose.Schema(
     ],
   },
   {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
+    timestamps: true
   }
 );
 
